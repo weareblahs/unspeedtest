553e5346e613643f7435494313ab6d71bd342643
weareblahs <tanyuxuan2005@gmail.com>
2023-05-28 12:04:00 +0800

    wondering how many commits ill do to write readme. too much bugs

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo 'https://www.speedtest.net/apps/cli#Install_Options to install Speedtest, t
 echo 'run for the first time and accept EULA in order to let unspeedtest work'
 echo 'properly.'
 echo ''
-read -p "Press any key to start installation of unspeedtest."
+read -n 1 -s -r -p "Press any key to start installation of unspeedtest."
 echo "Checking if required software is installed..."
 sudo apt install python apache2
 echo "Installing Python requirements..."
@@ -14,6 +14,7 @@ pip install -r requirements.txt
 ln -s $(pwd)/apache /var/www/html/unspeedtest
 echo $(pwd) >> dir
 chmod +x *.sh
+echo "Setting up cronjob..."
 crontab cron
 ./clearresults.sh
 echo "unspeedtest is accessible via http://[LOCAL_IP]/unspeedtest."
