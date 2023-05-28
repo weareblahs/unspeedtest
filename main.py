8b11ec428108a0fed64d3063418513a518c27329
weareblahs <tanyuxuan2005@gmail.com>
2023-05-28 11:53:42 +0800

    pre-readme fixes and stuff

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-suffixes = ['bps', 'Kbps', 'Mbps', 'Gbps']
-def convert(nbytes):
-    i = 0
-    while nbytes >= 1024 and i < len(suffixes)-1:
-        nbytes /= 1024.
-        i += 1
-    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
-    return '%s %s' % (f, suffixes[i])
-def convert_nosuffix(nbytes):
-    i = 0
-    while nbytes >= 1024:
-        nbytes /= 1024.
-        i += 1
-    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
-    return f
-
 # Importing stuffs
 from datetime import date
 import os
@@ -33,16 +17,20 @@ with open(json_out_fn, 'r') as file:
 import json
 data2 = json.loads(data)
 
-download = convert(data2["download"]["bytes"])
-upload = convert(data2["upload"]["bytes"])
-dl_chart = convert_nosuffix(data2["download"]["bytes"])
-ul_chart = convert_nosuffix(data2["upload"]["bytes"])
+import math
+
+dl = round((data2["download"]["bandwidth"])/125000,2) 
+ul = round((data2["upload"]["bandwidth"])/125000,2) 
+download = dl, " Mbps"
+upload = ul, " Mbps"
 ping_lat = str(data2["ping"]["latency"]) + "ms"
 sturl = '<a href="' + str(data2["result"]["url"]) + '" class="btn btn-primary btn-sm btn-block">Result on speedtest.net</a>'
+testfrom = str(data2["isp"])
+testto = data2["server"]["name"] + ", " + data2["server"]["location"] + ", " + data2["server"]["country"]
 testtime0 = datetime.datetime.now()
 testtime = testtime0.strftime("%Y-%m-%d %H:%M:%S")
 from tabulate import tabulate
-table = [[download,upload,ping_lat,sturl,testtime]]
+table = [[testfrom,testto,download,upload,ping_lat,sturl,testtime]]
 htmltext = tabulate(table, tablefmt='html')
 htmltext = tabulate(table, tablefmt='html') + "\n<!--STDATA-->"
 # Code from https://wiki.python.org/moin/EscapingHtml#Unescaping_HTML
@@ -56,12 +44,16 @@ def format(s):
    # this has to be last:
    s = s.replace("&amp;", "&")
    s = s.replace("&quot;", '"')
+   s = s.replace(", &#x27; Mbps&#x27;",' Mbps')
+   s = s.replace("'",'')
+   s = s.replace('<td>(','<td>')
+   s = s.replace(')</td>','</td>')
    return s
 
 # Create chart image for today's speedtest
 import matplotlib.pyplot as plt
 name = ['','Upload','Download']
-data = ['0',ul_chart, dl_chart]
+data = ['0',str(round((data2["download"]["bandwidth"])/125000,2)), str(round((data2["upload"]["bandwidth"])/125000,2))]
 colors = ['red', 'green']
 plt.bar(name,data,color=colors)
 plt.title('Speedtest result on ' + str(datetime.date.today()))
