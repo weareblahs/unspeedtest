097658fcb8e64068d417a64cf8f14ccd9907147d
weareblahs <tanyuxuan2005@gmail.com>
2023-05-28 12:01:09 +0800

    wondering how many commits ill do to write readme. too much bugs

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
-echo "If you didn't install Speedtest yet (or you installed speedtest-cli instead of
+echo "unspeedtest by Tan (weareblahs)"
+echo ''
+echo "If you didn't install Speedtest yet (or you installed speedtest-cli instead of"
 echo 'the official Ookla one), Please follow the steps on'
 echo 'https://www.speedtest.net/apps/cli#Install_Options to install Speedtest, then'
 echo 'run for the first time and accept EULA in order to let unspeedtest work'
 echo 'properly.'
-pause
+echo ''
+read -p "Press any key to start installation of unspeedtest."
 echo "Checking if required software is installed..."
 sudo apt install python apache2
 echo "Installing Python requirements..."
@@ -12,4 +15,5 @@ ln -s $(pwd)/apache /var/www/html/unspeedtest
 echo $(pwd) >> dir
 chmod +x *.sh
 crontab cron
+./clearresults.sh
 echo "unspeedtest is accessible via http://[LOCAL_IP]/unspeedtest."
